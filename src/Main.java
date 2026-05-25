394052ecc8d52e9e75165e9f8d95eafc27c6e337
Dhruva Hegde <dhruvahegde2021@gmail.com>
2026-05-25 10:08:10 +0530

    Added multithreading feature

--- src/Main.java
+++ src/Main.java
@@ -43,56 +43,82 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(8080);
         System.out.println("Sever started on port 8080");
-        while(true)
-        {
-            Socket client=server.accept();
-            System.out.println("Client Connected");
-            var input=client.getInputStream();
-            var reader=new BufferedReader(new InputStreamReader(input));
-            String firstLine=reader.readLine();
-            String[] words=firstLine.split(" ");
-            String method=words[0];
-            String path=words[1];
-            if(path.equals("/favicon.ico"))
+        while (true) {
+            Socket client = server.accept();
+
+            Thread thread = new Thread(() ->
             {
-                client.close();
-                continue;
-            }
-            System.out.println(method);
-            System.out.println(path);
+                try {
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("Client Connected");
+                    var input = client.getInputStream();
+                    var reader = new BufferedReader(
+                            new InputStreamReader(input)
+                    );
+                    String firstLine = reader.readLine();
+                    String[] words = firstLine.split(" ");
+                    String method = words[0];
+                    String path = words[1];
 
+                    if (path.equals("/favicon.ico")) {
+                        client.close();
+                        return;
+                    }
 
-            String line;
-            while((line= reader.readLine())!=null && !line.isEmpty()) {
-                System.out.println(line);
-            }
-            var output=client.getOutputStream();
-            byte[] body;
-            String status;
-            String ContentType = "";
-            String fullPath=getFilePath(path);
-            String filename = Paths.get(fullPath).getFileName().toString();
-            System.out.println(fullPath);
+                    System.out.println(method);
+                    System.out.println(path);
 
-            try{
-                ContentType=getContentType(filename);
-                body=Files.readAllBytes(Paths.get(fullPath));
-                status="200 OK";
-            }
-            catch (Exception e) {
-                body=Files.readAllBytes(Paths.get("public/error.html"));
-                status="404 Not Found";
-            }
-            String header=
-                    "HTTP/1.1 "+ status +"\r\n" +
-                    "Content-Type:"+ContentType+"\r\n" +
-                    "Content-Length:"+body.length+"\r\n"+
-                    "\r\n";
+                    String line;
 
-            output.write(header.getBytes());
-            output.write(body);
-            output.flush();
-            client.close();
+                    while ((line = reader.readLine()) != null
+                            && !line.isEmpty()) {
+                        System.out.println(line);
+                    }
+
+                    var output = client.getOutputStream();
+                    byte[] body;
+                    String status;
+                    String contentType = "";
+                    String fullPath = getFilePath(path);
+                    String filename =
+                            Paths.get(fullPath)
+                                    .getFileName()
+                                    .toString();
+
+                    System.out.println(fullPath);
+
+                    try {
+                        contentType = getContentType(filename);
+                        body = Files.readAllBytes(
+                                Paths.get(fullPath)
+                        );
+
+                        status = "200 OK";
+                    } catch (Exception e) {
+                        body = Files.readAllBytes(
+                                Paths.get("public/error.html")
+                        );
+
+                        status = "404 Not Found";
+
+                        contentType = "text/html";
+                    }
+
+                    String header =
+                            "HTTP/1.1 " + status + "\r\n" +
+                                    "Content-Type: " + contentType + "\r\n" +
+                                    "Content-Length: " + body.length + "\r\n" +
+                                    "\r\n";
+
+                    output.write(header.getBytes());
+                    output.write(body);
+                    output.flush();
+                    client.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
         }
     }
 }
